192e1d62060d8e49fe576f72f9a016ff2cc5996d
xbiwas <biwas2059@gmail.com>
2024-01-21 22:45:58 +0545

    clerk auth

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div className="">
+      <SignUp />
+    </div>
+  );
+};
+export default page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello Admin</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+export default Home;
